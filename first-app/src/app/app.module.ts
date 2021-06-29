886d6eff905b58ec19ab5c455d459c73983796a6
nagarajuthandu <nagaraju.thandu@cmritonline.ac.in>
2021-06-29 11:46:34 +0530

    two way binding with ngModel

---
+++ first-app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import {FormsModule} from '@angular/forms';
+import { PostListComponent } from './posts/post-list/post-list.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostCreateComponent,
+    PostListComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
